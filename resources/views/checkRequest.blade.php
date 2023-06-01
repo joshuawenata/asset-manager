6a7730808f445cffeede78d242e7be9078da313a
joshuawenata <joshuawenata@gmail.com>
2023-06-01 18:01:29 +0700

    pinjam cuma barang divisi masing-masing

--- resources/views/checkRequest.blade.php
+++ resources/views/checkRequest.blade.php
@@ -20,41 +20,6 @@
 @endsection
 
 @section('content')
-
-    {{--    modal divs --}}
-    <div class="modal fade" id="see" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <h5 class="modal-title">Divisi</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form method="post" action="{{ route('createRequest') }}">
-                    <div class="modal-body">
-                        @csrf
-                        @if (session('datetimes'))
-                            <input type="hidden" name="datetimes" value="{{ session('datetimes') }}">
-                        @else
-                        @endif
-                        @if (session('data'))
-                            <select class="form-select" name="division_id" id="division_id">
-                                @foreach (session('data') as $index => $item)
-                                    <option value="{{ $item->id }}">{{ $item->name }}</option>
-                                @endforeach
-                            </select>
-                        @endif
-
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">OK</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
@@ -65,9 +30,7 @@
 
                     <div class="card-body">
 
-
-
-                        <form method="POST" action="{{ route(\App\Models\User::getRolePage()) }}">
+                        <form method="POST" action="{{ route('createRequest') }}">
                             @csrf
 
                             <div class="row mb-3">
@@ -113,5 +76,4 @@
             </div>
         </div>
     </div>
-
 @endsection

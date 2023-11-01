9caa08bf8b1fc033ae5afe1cd8d2afc5ad7614d0
joshuawenata <joshuawenata@gmail.com>
2023-11-01 09:14:50 +0700

    change app.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import './bootstrap';
 import 'flowbite';
-
+import '../../public/css/app.css'; 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite('public/css/app.css')
+    @vite('resources/js/app.js')
 
     <title>Inventory Management BDG</title>
 

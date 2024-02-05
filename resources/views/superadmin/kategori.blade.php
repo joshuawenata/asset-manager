1d61090afac0a3354812dc9a5f3c651d8c5b7b34
joshuawenata <joshuawenata@gmail.com>
2024-02-05 10:06:11 +0700

    ganti label kategori jadi jenis

--- resources/views/superadmin/kategori.blade.php
+++ resources/views/superadmin/kategori.blade.php
@@ -76,7 +76,7 @@
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th class="px-6 py-3" scope="col">No</th>
-                                    <th class="px-6 py-3" scope="col">Kategori Barang</th>
+                                    <th class="px-6 py-3" scope="col">Jenis Barang</th>
                                     <th class="px-6 py-3" scope="col">Action</th>
                                 </tr>
                             </thead>
